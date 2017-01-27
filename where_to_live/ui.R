a47e497fa2b5706f5dd5d8549908c0098b13eb63
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-01-27 12:57:58 -0500

    Fixes the documentation
    I thought there was some cool/easy way to do it.

--- where_to_live/ui.R
+++ where_to_live/ui.R
@@ -9,6 +9,9 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+      h4("This web application helps you decide what state to live in based on its average temperature and average amount of sun during the day."),
+      h4("Move the sliders on the left for temperature and percentage of sunniness to set ranges that you deem acceptable to live in. The map on the right will update, highlighing the states whose averages fall within your ranges. Then all that's left is for you to pack up and move!"),
+      h4("Data for this application comes from the NOAA National Climatic Data Center of the United States."),
       sliderInput("temperature", label = h3("Acceptable Temperature"), min = 40, 
                   max = 71, value = c(40, 71)),
       sliderInput("sun", label = h3("Acceptable Percentage of Sunniness"), min = 46, 
